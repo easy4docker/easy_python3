7740fb63ea7113e2e74f69678fb75c832359b7ab
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 11:28:48 -0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-SCR_DIR=$(cd `dirname $0` && pwd)
-docker container stop easydocker-python-container
-docker container rm easydocker-python-container
-docker image rm easydocker-python-image
-
-docker build -f dockerFile -t easydocker-python-image .
-docker run -it  --name easydocker-python-container -v "${SCR_DIR}":/var/app easydocker-python-image 
