df9c329bfdfc2875f74cdbc189c2a2e72792a462
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 10:55:14 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+docker container stop easydocker-python-container
+docker container rm easydocker-python-container
+docker image rm easydocker-python-image
+
+docker build -f dockerFile -t easydocker-python-image .
+docker run -it  --name easydocker-python-container -v "${SCR_DIR}":/var/app easydocker-python-image 
