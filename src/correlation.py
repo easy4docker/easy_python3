d6916269cc90df7772d6e1d4318c715ff09b5acd
Sui Feng Xu <easy4docker@gmail.com>
2020-12-28 17:43:04 -0800

    Fix bugs

--- src/correlation.py
+++ src/correlation.py
@@ -1,25 +1,28 @@
 import sys
 import pandas as pd
 import numpy as np
+
 from datetime import date
+from os import path
 
 def validate(ticker):
-    if len(ticker) == 0:
-        raise IOError("argv cannot be empty")
+    if not path.isfile("/var/appData/{0}.csv".format(ticker)):
+        raise IndexError(ticker + " ticker is not indexed")
     return ticker
 
 def extract(fileName):
     return pd.read_csv(fileName)
 
 def correlateStocks(stockDF1, stockDF2):
-    openCorrelation = np.correlate(stockDF1['Open'], stockDF2['Open'])
-    closeCorrelation = np.correlate(stockDF1['Close'], stockDF2['Close'])
-    highCorrelation = np.correlate(stockDF1['High'], stockDF2['High'])
-    lowCorrelation = np.correlate(stockDF1['Low'], stockDF2['Low'])
-    volumeCorrelation = np.correlate(stockDF1['Volume'], stockDF2['Volume'])
+    data = dict()
+    data['Open'] = np.correlate(stockDF1['Open'], stockDF2['Open'])
+    data['Close'] = np.correlate(stockDF1['Close'], stockDF2['Close'])
+    data['High'] = np.correlate(stockDF1['High'], stockDF2['High'])
+    data['Low'] = np.correlate(stockDF1['Low'], stockDF2['Low'])
+    data['Volume'] = np.correlate(stockDF1['Volume'], stockDF2['Volume'])
 
-    return pd.DataFrame(np.array([openCorrelation, closeCorrelation, highCorrelation, lowCorrelation, volumeCorrelation]),
-                        columns=['Open', 'Close', 'High', 'Low', 'Volume'])
+    return pd.DataFrame(data)
+
 def export(dataFrame, ticker1, ticker2):
     dataFrame.to_csv(path_or_buf="/var/appData/correlation_{0}_{1}_{2}.csv".format(ticker1, ticker2, date.today().strftime("%b-%d-%Y")), index=False)
 
@@ -28,7 +31,7 @@ if __name__ == '__main__':
     ticker2 = validate(sys.argv[2])
 
     stock1 = extract("/var/appData/{0}.csv".format(ticker1))
-    stock2 = extract("/var/appData/{1}.csv".format(ticker2))
+    stock2 = extract("/var/appData/{0}.csv".format(ticker2))
 
     dataFrame = correlateStocks(stock1, stock2)
-    export(ticker1, ticker2, dataFrame)
+    export(dataFrame, ticker1, ticker2)
